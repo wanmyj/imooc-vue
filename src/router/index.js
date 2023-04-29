b154cb2714e789d324bc882045cb8661e15b9427
wanmyj <wanmyj@hotmail.com>
2023-04-29 23:04:57 +0800

    login showed

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const publicRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: publicRoutes
+})
+
+export default router
