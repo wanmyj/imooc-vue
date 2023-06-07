b6d0a0474adf0c55479bb7d22fe780d750ee4b99
wanmyj <wanmyj@hotmail.com>
2023-06-07 22:54:24 +0800

    动态面包屑

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,50 +1,55 @@
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item :to="{path: '/'}">首页</el-breadcrumb-item>
-    <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-    <el-breadcrumb-item>
-      <span class="no-redirect">活动详情</span>
+    <el-breadcrumb-item v-for="(item, index) in breadcrumbData" :key="item.path">
+      <!-- 不可点击 -->
+      <span class="no-redirect" v-if="index === breadcrumbData.length - 1">
+        {{ item.meta.title }}
+      </span>
+      <!-- 可点击 -->
+      <span class="redirect" v-else @click="onLinkClick(item)">
+        {{ item.meta.title }}
+      </span>
     </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script setup>
-// import { ref, watch } from 'vue'
-// import { useRoute, useRouter } from 'vue-router'
-// import { useStore } from 'vuex'
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useStore } from 'vuex'
 
-// const route = useRoute()
-// // 生成数组数据
-// const breadcrumbData = ref([])
-// const getBreadcrumbData = () => {
-//   breadcrumbData.value = route.matched.filter(
-//     (item) => item.meta && item.meta.title
-//   )
-//   console.log(breadcrumbData.value)
-// }
-// // 监听路由变化时触发
-// watch(
-//   route,
-//   () => {
-//     getBreadcrumbData()
-//   },
-//   {
-//     immediate: true
-//   }
-// )
+const route = useRoute()
+// 生成数组数据
+const breadcrumbData = ref([])
+const getBreadcrumbData = () => {
+  breadcrumbData.value = route.matched.filter(
+    (item) => item.meta && item.meta.title
+  )
+  console.log("breadcrumbData.value")
+  console.log(breadcrumbData.value)
+}
+// 监听路由变化时触发
+watch(
+  route,
+  () => {
+    getBreadcrumbData()
+  },
+  {
+    immediate: true
+  }
+)
 
-// // 处理点击事件
-// const router = useRouter()
-// const onLinkClick = (item) => {
-//   console.log(item)
-//   router.push(item.path)
-// }
+// 处理点击事件
+const router = useRouter()
+const onLinkClick = (item) => {
+  console.log(item)
+  router.push(item.path)
+}
 
 // // 将来需要进行主题替换，所以这里获取下动态样式
-// const store = useStore()
-// // eslint-disable-next-line
-// const linkHoverColor = ref(store.getters.cssVar.menuBg)
+const store = useStore()
+// eslint-disable-next-line
+const linkHoverColor = ref(store.getters.cssVar.menuBg)
 </script>
 
 <style lang="scss" scoped>
@@ -59,14 +64,15 @@
     cursor: text;
   }
 
-//   .redirect {
-//     color: #666;
-//     font-weight: 600;
-//   }
+  .redirect {
+    color: #666;
+    font-weight: 600;
+    cursor: pointer;
+  }
 
-//   .redirect:hover {
-//     // 将来需要进行主题替换，所以这里不去写死样式
-//     color: v-bind(linkHoverColor);
-//   }
+  .redirect:hover {
+    // 将来需要进行主题替换，所以这里不去写死样式
+    color: v-bind(linkHoverColor);
+  }
 }
 </style>
