0ac203884fa8f0df66d0bbfa6673d3456e91e56f
wanmyj <wanmyj@hotmail.com>
2023-07-03 08:55:32 +0800

    主题色变更

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,7 @@
   <router-view />
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
+<script setup>
 </script>
 
 <style></style>
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="">
     {{ t('msg.test') }}
+
+    <el-pagination
+      :page-size="100"
+      layout="total, prev, pager, next"
+      :total="1000"
+    >
+    </el-pagination>
+
+    <el-row>
+      <el-button>Default</el-button>
+      <el-button type="primary">primary</el-button>
+      <el-button type="success">success</el-button>
+      <el-button type="info">info</el-button>
+      <el-button type="warning">warning</el-button>
+      <el-button type="danger">danger</el-button>
+    </el-row>
   </div>
 </template>
 
